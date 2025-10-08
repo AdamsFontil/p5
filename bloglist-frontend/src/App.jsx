434f35356425f9802e32c105e756874fbbd00f5f
Adams <adamsfuntil@gmail.com>
2025-10-08 09:10:30 -0400

    5.5

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -144,7 +145,8 @@ const notificationComp = () => (
       <h2>blogs</h2>
       {notification && notificationComp()}
       <p> {user.name} is logged in <button onClick={handleLogout}>logout</button></p>
-      <form onSubmit={handleCreate}>
+      <Togglable buttonLabel="create new blog" >
+        <form onSubmit={handleCreate}>
         <div>
           <label>
             title
@@ -171,6 +173,7 @@ const notificationComp = () => (
         </div>
         <button type="submit">create</button>
       </form>
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
