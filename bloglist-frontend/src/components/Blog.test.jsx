bf558759470f68bdde4a0032d8e08ea4a9cc9dbc
Adams <adamsfuntil@gmail.com>
2025-10-29 14:59:43 -0400

    5.14

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,14 +1,20 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+const blog = {
+  title: 'one more time',
+  author: 'hellas',
+  url: 'onemoretime.com',
+  likes: 0,
+  user: {
+    username: 'hellas',
+    name: 'arto hellas',
+  },
+}
+
+
 test('renders blog with author and title', () => {
-  const blog = {
-    title: 'vitetest Blog ',
-    author: 'hellas',
-    url: 'vitejs.com',
-  }
-
-
 
   const { container } = render(<Blog blog={blog} />)
 
@@ -24,4 +30,31 @@ test('renders blog with author and title', () => {
   )
   screen.debug(div)
   expect(div).toBeDefined()
+})
+
+test('clicking on view reveals url and likes', async () => {
+
+  const { container } = render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const div = container.querySelector('.blog')
+  expect(div).toHaveTextContent(
+    blog.title
+  )
+  expect(div).toHaveTextContent(
+    blog.author
+  )
+  expect(div).toHaveTextContent(
+    blog.url
+  )
+  expect(div).toHaveTextContent(
+    blog.likes
+  )
+  screen.debug(div)
+  expect(div).toBeDefined()
+
+
 })
