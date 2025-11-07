ab797a8ee142224666c3733d4e00e8b7cf9e7236
Adams <adamsfuntil@gmail.com>
2025-11-06 22:54:33 -0500

    5.18

--- playwright/tests/blogApp.spec.js
+++ playwright/tests/blogApp.spec.js
@@ -1,8 +1,17 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Hellas Testing',
+        username: 'tHellas',
+        password: '1235'
+      }
+    })
+
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
@@ -11,6 +20,27 @@ describe('Blog app', () => {
   await expect(page.getByLabel('Username')).toBeVisible()
   await expect(page.getByLabel('Password')).toBeVisible()
   await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
-
   })
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('tHellas')
+      await page.getByLabel('password').fill('1235')
+      await page.getByRole('button', {name:'login'}).click()
+
+      await expect(page.getByText('Hellas Testing is logged in')).toBeVisible()
+      await expect(page.getByText('Login succeeded tHellas is in')).toBeVisible()
+
+    })
+
+    test.only('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('tHellas')
+      await page.getByLabel('password').fill('wrong')
+      await page.getByRole('button', {name:'login'}).click()
+
+      await expect(page.getByText('Hellas Testing is logged in')).not.toBeVisible()
+      await expect(page.getByText('Login succeeded tHellas is in')).not.toBeVisible()
+      await expect(page.getByText('invalid username or password')).toBeVisible()
+
+    })
+})
 })
